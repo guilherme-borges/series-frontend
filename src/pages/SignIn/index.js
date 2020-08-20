f90c4b7a4324c56d5114b95eba08b6e42e92663c
Guilherme Borges <guilherme.borges.dev@gmail.com>
2020-08-20 20:50:44 -0300

    Adicionado funcionalidade de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,31 +1,61 @@
-import React from 'react';
-import './styles.css';
+import React, { useState } from "react";
+import { Link, useHistory } from 'react-router-dom';
+import "./styles.css";
+import api from '../../services/api';
+import { login } from "../../services/auth";
 
 export default function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const history = useHistory();
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    if (!username || !password) {
+      alert("Preencha todos os campos corretamente!");
+    } else {
+      try {
+        const response = await api.post("sessions", { username, password });
+        login(response.data.token);
+        history.push("/home");
+      } catch (error) {
+        console.log(error);
+        alert("Erro ao fazer login tente novamente.");
+      }
+    }
+  }
 
   return (
     <div className="container signin-container">
-      <div className="row">
-        <form>
-          <div className="row">
-            <div className="input-field" >
-              <input id="txtUsername" type="text" className="validate" />
-              <label for="txtUsername">Nome de Usuário</label>
-            </div>
-          </div>
-              
-          <div className="row">
-            <div className="input-field">
-              <input id="txtPassword" type="password" className="validate" />
-              <label for="txtPassword">Senha</label>
-            </div>
-          </div>
+      <form onSubmit={handleSignIn}>
+        <div className="form-group">
+          <label htmlFor="txtUsername">Nome de Usuário</label>
+          <input
+            id="txtUsername"
+            type="text"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
 
-          <div className="row">
-            <button className="col s12 btn waves-effect waves-light" type="submit" >Entrar</button>
-          </div>
-        </form>
-      </div>
+        <div className="form-group">
+          <label htmlFor="txtPassword">Senha</label>
+          <input
+            id="txtPassword"
+            type="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button className="btn btn-primary form-control" type="submit">
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import SignIn from './pages/SignIn';
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { isAuthenticated } from "./services/auth";
+
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 const Routes = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route path="/" exact component={SignIn}></Route>
-        </Switch>
-    </BrowserRouter>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact component={SignIn}></Route>
+      <PrivateRoute path="/home" component={Home}></PrivateRoute>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
